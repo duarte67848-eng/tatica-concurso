c70f0fa760214d1f89dc787875ce3f35308a46a0
User <user@example.com>
2026-05-09 20:20:10 -0300

    Remove app folder, fix config

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  images: { unoptimized: true },
 };
 
 export default nextConfig;
